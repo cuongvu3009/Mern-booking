55dad1d02744fef93852cf02fcab3a2dd59acb62
cuongvu3009 <cuongvu3009@gmail.com>
2022-11-29 03:29:19 +0200

    feat: add redux

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,13 @@ import Register from './pages/register/Register';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/hotels' element={<List />} />
-        <Route path='/hotels/:id' element={<Hotel />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/hotels' element={<List />} />
+      <Route path='/hotels/:id' element={<Hotel />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom';
+
+//redux
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
+//redux persist
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
+  </BrowserRouter>
 );
