6df22ea455a6e4c1045dcce35a324dd3690978ed
cuongvu3009 <cuongvu3009@gmail.com>
2022-11-30 00:14:46 +0200

    fix: serializableCheck redux resist

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -18,14 +18,16 @@ router.post('/', verifyAdmin, createHotel);
 
 //UPDATE
 router.put('/:id', verifyAdmin, updateHotel);
+
 //DELETE
 router.delete('/:id', verifyAdmin, deleteHotel);
+
 //GET
-
 router.get('/find/:id', getHotel);
-//GET ALL
 
+//GET ALL
 router.get('/', getHotels);
+
 router.get('/countByCity', countByCity);
 router.get('/countByType', countByType);
 router.get('/room/:id', getHotelRooms);
--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -14,19 +14,21 @@ const {
 } = require('../utils/verifyToken.js');
 
 const router = express.Router();
+
 //CREATE
 router.post('/:hotelid', verifyAdmin, createRoom);
 
 //UPDATE
 router.put('/availability/:id', verifyToken, updateRoomAvailability);
 router.put('/:id', verifyAdmin, updateRoom);
+
 //DELETE
 router.delete('/:id/:hotelid', verifyAdmin, deleteRoom);
+
 //GET
-
 router.get('/:id', getRoom);
-//GET ALL
 
+//GET ALL
 router.get('/', getRooms);
 
 module.exports = router;
