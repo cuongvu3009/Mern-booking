9a9fc45f90fb7ee47ab55438f590825d241c5b6a
cuongvu3009 <cuongvu3009@gmail.com>
2022-11-29 16:51:42 +0200

    feat: retouch logo, conditional render button nav

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,22 +1,47 @@
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../redux/userSlice';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className='navbar'>
       <div className='navContainer'>
-        <span className='logo'>Booking</span>
+        <span className='logo'>
+          <Link to='/' className='styledLink'>
+            <h1>Booking</h1>
+          </Link>
+        </span>
         <div className='navItems'>
-          <button className='navButton'>
-            <Link to='/register' className='styledLink'>
-              Register
-            </Link>
-          </button>
-          <button className='navButton'>
-            <Link to='/login' className='styledLink'>
-              Login
-            </Link>
-          </button>
+          {currentUser ? (
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <button className='navButton'>
+                <Link to='/' className='styledLink'>
+                  Manage Account
+                </Link>
+              </button>
+              <button className='navButton' onClick={() => dispatch(logout())}>
+                Logout
+              </button>
+            </div>
+          ) : (
+            <>
+              <button className='navButton'>
+                <Link to='/register' className='styledLink'>
+                  Register
+                </Link>
+              </button>
+              <button className='navButton'>
+                <Link to='/login' className='styledLink'>
+                  Login
+                </Link>
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
