4c4895df44cfd73552a28b0e0576883cfa9285d7
cuongvu3009 <cuongvu3009@gmail.com>
2022-11-29 03:36:50 +0200

    feat: connect login ui with api

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { loginSuccess, loginFailure, loginStart } from '../../redux/userSlice';
+import axios from 'axios';
 import './login.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    dispatch(loginStart());
+    try {
+      const res = await axios.post('/api/v1/auth/login', {
+        username,
+        password,
+      });
+      setUsername('');
+      setPassword('');
+      dispatch(loginSuccess(res.data));
+      setTimeout(() => {
+        navigate('/');
+      }, 200);
+    } catch (error) {
+      dispatch(loginFailure());
+      console.log(error);
+    }
+  };
+
   return (
     <div className='login'>
-      <div className='lContainer'>
+      <form className='lContainer' onSubmit={handleLogin}>
         <input
           type='text'
           placeholder='username'
@@ -23,14 +49,17 @@ const Login = () => {
           id='password'
           className='lInput'
         />
-        <button className='lButton'>Login</button>
-        <p>
-          New member? <Link to='/register'>Register</Link>
-        </p>
-        <p>
-          Just looking around? <Link to='/'>Back home</Link>
-        </p>
-      </div>
+        <button className='lButton' type='submit'>
+          Login
+        </button>
+      </form>
+
+      <p>
+        New member? <Link to='/register'>Register</Link>
+      </p>
+      <p>
+        Just looking around? <Link to='/'>Back home</Link>
+      </p>
     </div>
   );
 };
